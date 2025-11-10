7ddfc5ad4c625bf9630fb75416fe4275dee8b995
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2025-11-10 11:12:45 -0800

    isRancherPrime in nav hooks

--- pkg/virtual-clusters/index.ts
+++ pkg/virtual-clusters/index.ts
@@ -6,6 +6,8 @@ import virtualClusterRouting from './routes'
 import virtualClusterAdminRole from './resources/virtual-cluster-admin-role.json'
 import virtualClusterPolicyAdminRole from './resources/virtual-cluster-policy-admin-role.json'
 import { MANAGEMENT } from '@shell/config/types';
+import versions from '@shell/utils/versions';
+import { isRancherPrime } from '@shell/config/version';
 
 
 
@@ -33,59 +35,31 @@ export default function(plugin: IPlugin): void {
   // Built-in icon
   plugin.metadata.icon = require('./assets/icon-k3k.svg');
 
-//TODO nb only on prime
   plugin.addNavHooks(undefined, undefined, undefined, async(store: any)=>{
-    await store.dispatch('management/loadSchemas')
-    await store.dispatch('rancher/loadSchemas')
+    await versions.fetch({ store: store });
 
-    const roles = await store.dispatch('management/findAll', {type: MANAGEMENT.ROLE_TEMPLATE})
-console.log('*** roles ', roles)
-    const vcAdminRole = !!roles.filter((r:any)=>r.metadata.labels?.['management.cattle.io/ui-role-name'] === virtualClusterAdminRole.metadata.name)
-    console.log('*** found admin role ', vcAdminRole)
-    if(!vcAdminRole){
-      const newAdminRole = await store.dispatch('management/create', {type: MANAGEMENT.ROLE_TEMPLATE, ...virtualClusterAdminRole})
+    if(isRancherPrime()){
+      await store.dispatch('management/loadSchemas')
+      await store.dispatch('rancher/loadSchemas')
 
-      await newAdminRole.save()
+      const roles = await store.dispatch('management/findAll', {type: MANAGEMENT.ROLE_TEMPLATE})
+
+      const vcAdminRole = !!roles.filter((r:any)=>r.metadata.labels?.['management.cattle.io/ui-role-name'] === virtualClusterAdminRole.metadata.name)
+
+      if(!vcAdminRole){
+        const newAdminRole = await store.dispatch('management/create', {type: MANAGEMENT.ROLE_TEMPLATE, ...virtualClusterAdminRole})
+
+        await newAdminRole.save()
+      }
+
+      const vcPolicyAdminRole = !!roles.filter((r:any)=>r.metadata.labels?.['management.cattle.io/ui-role-name'] === virtualClusterPolicyAdminRole.metadata.name)
+
+      if(!vcPolicyAdminRole){
+        const newPolicyAdminRole = await store.dispatch('management/create', {type: MANAGEMENT.ROLE_TEMPLATE, ...virtualClusterPolicyAdminRole})
+
+        await newPolicyAdminRole.save()
+      }
     }
-
-    const vcPolicyAdminRole = !!roles.filter((r:any)=>r.metadata.labels?.['management.cattle.io/ui-role-name'] === virtualClusterPolicyAdminRole.metadata.name)
-    console.log('*** found policy role ', vcPolicyAdminRole)
-
-    if(!vcPolicyAdminRole){
-      const newPolicyAdminRole = await store.dispatch('management/create', {type: MANAGEMENT.ROLE_TEMPLATE, ...virtualClusterPolicyAdminRole})
-
-      await newPolicyAdminRole.save()
-    }
-
-    // const role = await store.dispatch('management/findLabelSelector', {
-    //   type: MANAGEMENT.ROLE_TEMPLATE,
-    //   matching: {labelSelector:{ matchLabels: virtualClusterAdminRole.metadata.labels}}
-    // })
-
-    // const forUrl = encodeURIComponent("management.cattle.io/ui-role-name=virtual-cluster-admin")
-    // // console.log(forUrl)
-
-    // await store.dispatch('management/request', {method: 'GET', url: `/v1/management.cattle.io.roletemplates?labelSelector=${forUrl}`})
-
-    // console.log('*** role(s) found: ', role)
-
-    // const newRole = await store.dispatch('management/create', {type: MANAGEMENT.ROLE_TEMPLATE, ...virtualClusterAdminRole})
-
-    // // await newRole.save()
-
-    // const norman = await newRole.norman;
-
-    // for (const rule of norman.rules) {
-    //   if (rule.nonResourceURLs && rule.nonResourceURLs.length) {
-    //     delete rule.resources;
-    //     delete rule.apiGroups;
-    //   } else {
-    //     delete rule.nonResourceURLs;
-    //   }
-    // }
-    // norman.labels = newRole.metadata.labels
-
-    // return norman.save();
   })
 
 }
