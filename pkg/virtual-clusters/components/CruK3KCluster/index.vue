3e491f9448797a2bbb2f135b46ccb63bada8b853
Nancy Butler <42977925+mantis-toboggan-md@users.noreply.github.com>
2026-04-08 11:22:05 -0700

    update yarn lock, ux feedback

--- pkg/virtual-clusters/components/CruK3KCluster/index.vue
+++ pkg/virtual-clusters/components/CruK3KCluster/index.vue
@@ -664,7 +664,7 @@ export default {
       <Tab
         v-if="!policy"
         name="affinity"
-        label-key="k3k.policy.tabs.scheduling"
+        label-key="k3k.policy.tabs.topology"
         :weight="9"
       >
         <Affinity
